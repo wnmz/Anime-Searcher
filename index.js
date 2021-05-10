c57e40091ae969c654d1589c1d435588291fbbd5
Daniil Parkov <damnwnm@gmail.com>
2021-05-10 21:24:11 +0600

    Timely rolling back to old client structure.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const { Searcher } = require('./app')
-const client = new Searcher()
-client.start()
+const config = require('./config.js')
 
+const { ShardingManager } = require('discord.js');
+const shard = new ShardingManager('./app.js', {
+  token: config.token,
+  shardCount: 'auto' ,
+});
 
+shard.spawn('auto', 0 , -1);
+
+shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.totalShards}`));
