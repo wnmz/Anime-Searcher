57b8cc446c884a62173a6797a7cbe9e5956a6eb6
wnmz <56759438+wnmz@users.noreply.github.com>
2021-07-05 15:25:13 +0600

    Update antispam.js

--- modules/antispam.js
+++ modules/antispam.js
@@ -3,30 +3,32 @@ const interval = 10000; // MS
 const maxMsgsInInterval = 3;
 
 //TODO: Make this better, looks awful.
-export function checkUser(msg) {
-    let userLogs = logs[msg.author.id];
-    if (userLogs) {
-        userLogs.messageCount += 1;
-        let timeDiff = msg.createdTimestamp - userLogs.lastMessageTimestamp;
-        if (timeDiff < interval) {
-            if (userLogs.messageCount != 1 && userLogs.messageCount > maxMsgsInInterval) {
-                msg.channel.send(`You're sending messages too quick. Try again in: \`${((interval - timeDiff) / 1000).toFixed(2)}s.\``)
-                    .then(msg => {
-                        msg.delete({
-                            timeout: 5000
-                        });
-                    });
-                return true;
+module.exports = {
+    checkUser: (msg) => {
+        let userLogs = logs[msg.author.id]
+        if (userLogs) {
+            userLogs.messageCount += 1;
+            let timeDiff = msg.createdTimestamp - userLogs.lastMessageTimestamp;
+            if (timeDiff < interval) {
+                if (userLogs.messageCount != 1 && userLogs.messageCount > maxMsgsInInterval) {
+                    msg.channel.send(`You're sending messages too quick. Try again in: \`${((interval - timeDiff)/1000).toFixed(2)}s.\``)
+                        .then(msg => {
+                            msg.delete({
+                                timeout: 5000
+                            })
+                        })
+                    return true;
+                }
+            } else {
+                delete logs[msg.author.id];
             }
         } else {
-            delete logs[msg.author.id];
+            logs[msg.author.id] = {
+                lastMessageTimestamp: msg.createdTimestamp,
+                messageCount: 1,
+            }
         }
-    } else {
-        logs[msg.author.id] = {
-            lastMessageTimestamp: msg.createdTimestamp,
-            messageCount: 1,
-        };
-    }
 
-    return false;
+        return false;
+    }
 }
