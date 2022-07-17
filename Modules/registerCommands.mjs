aba56116c500ab746f74a17fad306ea768d84215
wnmz <56759438+wnmz@users.noreply.github.com>
2022-07-17 21:48:13 +0700

    Interaction registration fix

--- Modules/registerCommands.mjs
+++ Modules/registerCommands.mjs
@@ -13,12 +13,8 @@ export const registerCommands = (clientId) => {
 
 		const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
-		rest.put(Routes.applicationCommands(clientId), { body: JSONCommands })
+		rest.put(Routes.applicationCommands(clientId), { body: [...JSONCommands, ...JSONContextMenuInteractions] })
 			.then(() => console.log('Successfully registered application commands.'))
-			.catch(console.error);
-
-		rest.put(Routes.applicationCommands(clientId), { body: JSONContextMenuInteractions })
-			.then(() => console.log('Successfully registered application context menu interactions.'))
 			.catch(console.error);
 	}
 	catch (e) {
