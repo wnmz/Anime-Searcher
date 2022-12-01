cacc9e1699ae799f6d5e934b2d5ab8d443c36f10
Daniil Parkov <56759438+wnmz@users.noreply.github.com>
2022-12-01 13:08:20 +0700

    reppl.it compatibility test

--- index.mjs
+++ index.mjs
@@ -24,3 +24,10 @@ shard.spawn({ amount: shard.totalShards, delay: 5500, timeout: 50000 });
 shard.on('shardCreate', sh => {
 	console.log(`[SHARD] Shard ${sh.id} was created.`);
 });
+
+// reppl.it compatibility test
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(request, response){ response.send(`Hello!`) });
